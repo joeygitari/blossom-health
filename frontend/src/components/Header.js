2586d42e98665872495afcb5a82d27880b763e83
Njihia Maureen <maureennjihia468@gmail.com>
2024-03-13 19:32:33 +0300

    Attempting homepage

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,64 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {NavLink} from "react-router-dom";
 import Logo from "../assets/images/logo.png"
 
 
 const Header = () => {
-  const [activeLink, setActiveLink] = useState('home');
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const handleSetActiveLink = (value) => {
-    setActiveLink(value);
-  };
-
   return (
-    // <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
-    //   <div className="container">
-    //     <div className="navbar-brand">
-    //       <div className="logo">
-    //         BlossomHealth<b>❀</b>
-    //       </div>
-    //     </div>
-    //     <button className="navbar-toggler" type="button" aria-label="Toggle navigation">
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div className="navbar-collapse">
-    //       <ul className="navbar-nav">
-    //         <li className="nav-item">
-    //           <a href="#home" className={`nav-link ${activeLink === 'home' ? 'active' : ''}`} onClick={() => handleSetActiveLink('home')}>Home</a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#aboutUs" className={`nav-link ${activeLink === 'aboutUs' ? 'active' : ''}`} onClick={() => handleSetActiveLink('skills')}>About Us</a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#services" className={`nav-link ${activeLink === 'services' ? 'active' : ''}`} onClick={() => handleSetActiveLink('projects')}>Services</a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#contact" className={`nav-link ${activeLink === 'connect' ? 'active' : ''}`} onClick={() => handleSetActiveLink('contact')}>Contact</a>
-    //         </li>
-    //       </ul>
-    //       <div className="navbar-text">
-    //         <button className="vvd"><span>Register</span></button>
-    //         <button className="vvd"><span>Login</span></button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </nav>
-
     <nav className="container  mx-auto bg-white border-gray-200 ">
             <div className=" flex flex-wrap items-center justify-between py-4">
                 <div className="flex">
@@ -75,7 +21,7 @@ const Header = () => {
                         <a href="/signup">Sign up</a>
                     </button> */}
                     <div
-                        className="font-poppins bg-[#FF8585] -pr-40 w-[111px] h-[49px] font-normal rounded-full text-white text-[18px] text-center px-4 py-2 cursor-pointer md:block hidden">
+                        className="font-poppins bg-[#FF8585] -pr-40 w-[147px] h-[50px] font-normal rounded-full text-white text-[16px] text-center px-4 py-3 cursor-pointer md:block hidden">
                         <a href="/signup">Register</a>
                     </div>
 
