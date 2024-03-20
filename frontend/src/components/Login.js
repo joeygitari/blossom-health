5ea8988c04aa36ed0c086f74aa0aba062e3a3cf9
Njihia Maureen <maureennjihia468@gmail.com>
2024-03-20 23:09:18 +0300

    Complete login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Logo from "../assets/images/logo.png";
+import LoginImg from "../assets/images/Signin.png";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
-    return(
-        <div></div>
+    return (
+        <section>
+            <div className="min-h-screen grid md:grid-cols-2">
+                <div className="p-10 bg-[#F7FAFC]">
+                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse cursor-pointer">
+                        <img className="h-16" alt="Logo" src={Logo}/>
+                        <p className="font-bold font-poppins py-4 text-[20px]">BlossomHealth</p>
+                    </a>
+
+                    <p className="font-poppins font-bold text-[48px] text-[#172048] mt-[2rem]">Sign in</p>
+
+                    <p className="font-poppins font-normal text-[18px] text-[#718096] mt-[2rem]">
+                        Don't have an account? <a href="/register"
+                                                  className="text-[#FF0000] font-medium underline hover:no-underline">Create
+                        now</a>
+                    </p>
+
+                    <LoginForm/>
+                </div>
+
+                <div className="hidden sm:hidden md:block">
+                    <img className="min-h-screen w-full" alt="Login" src={LoginImg}/>
+                </div>
+            </div>
+        </section>
     );
 }
 
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,8 +1,79 @@
 import React from "react";
+import Line3 from "../assets/images/Line3.png";
 
 const LoginForm = () => {
     return (
-        <></>
+        <form className="max-w-xl mt-[2rem]">
+            <div className="mb-5">
+                <label htmlFor="email" className="block mb-2 text-[18px] font-poppins font-medium text-[#718096]">
+                    E-mail
+                </label>
+                <input type="email" id="email"
+                       className="bg-[#F7FAFC] border border-[#CBD5E0] font-poppins font-normal text-[#4A5568] text-[16px] rounded-[12px] w-full p-3"
+                       placeholder="eg email@gmail.com" required/>
+            </div>
+
+            <div className="mb-5">
+                <label htmlFor="password" className="block mb-2 text-[18px] font-poppins font-medium text-[#718096]">
+                    Password
+                </label>
+                <input type="password" id="password"
+                       className="bg-[#F7FAFC] border border-[#CBD5E0] font-poppins font-normal text-[#4A5568] text-[16px] rounded-[12px] w-full p-3"
+                       placeholder="@#*%"
+                       required/>
+            </div>
+
+            <div className="flex items-center justify-between mb-5">
+                <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                        <input id="remember" type="checkbox" value=""
+                               className="w-4 h-4 border border-[#CFD9E0] rounded bg-white"
+                               required/>
+                    </div>
+
+                    <div className="">
+                        <label htmlFor="remember" className="ms-2 text-[16px] font-poppins font-normal text-[#718096]">
+                            Remember me
+                        </label>
+                    </div>
+                </div>
+                <a href="/forgot-password"
+                   className="text-[16px] font-poppins font-medium text-[#A19B9B] underline hover:no-underline dark:text-primary-500">
+                    Forgot password?
+                </a>
+            </div>
+
+            <button type="submit"
+                    className="mt-[2rem] text-[#F7FAFC] bg-[#FF8585] h-14 font-poppins font-semibold rounded-[20px] text-[20px] w-full px-5 py-2.5 text-center">
+                Sign in
+            </button>
+
+            <img src={Line3} className="mx-auto mt-[3rem]" alt="Line2"/>
+
+            <button
+                className="w-full mt-[3rem] h-14 px-6 border-2 border-[#CBD5E0] rounded-full transition duration-300 hover:border-blue-400">
+                <div className="relative flex items-center space-x-4 justify-center">
+                    <img src="https://www.svgrepo.com/show/475656/google-color.svg"
+                         className="absolute left-0 w-5" alt="google logo"/>
+                    <span
+                        className="block w-max font-poppins font-semibold tracking-wide text-[#67728A] text-[18px] transition duration-300 group-hover:text-blue-600 sm:text-base">
+                        Continue with Google
+                    </span>
+                </div>
+            </button>
+
+            <button
+                className="w-full mt-[3rem] h-14 px-6 border-2 border-[#CBD5E0] rounded-full transition duration-300 hover:border-blue-400">
+                <div className="relative flex items-center space-x-4 justify-center">
+                    <img src="https://www.svgrepo.com/show/448224/facebook.svg"
+                         className="absolute left-0 w-5" alt="google logo"/>
+                    <span
+                        className="block w-max font-poppins font-semibold tracking-wide text-[#67728A] text-[18px] transition duration-300 group-hover:text-blue-600 sm:text-base">
+                        Continue with Facebook
+                    </span>
+                </div>
+            </button>
+        </form>
     )
 }
 
