5bbf0cb4ca74b78c32b4c3b1f4c0fee7d14701e5
joeygitari <gitarijoanne@gmail.com>
2024-05-09 20:26:51 +0300

    symptoms on sidebar

--- frontend/src/components/dashboards/common/Sidebar/index.js
+++ frontend/src/components/dashboards/common/Sidebar/index.js
@@ -161,6 +161,22 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                             </SidebarLinkGroup>
                             {/* <!-- Menu Item Dashboard --> */}
 
+                            {/* <!-- Menu Item Medication --> */}
+                            {currentUser?.role === 'patient' && (
+                            <li>
+                                <NavLink
+                                    to="/patient-dashboard/symptoms"
+                                    className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-poppins font-medium text-black duration-300 ease-in-out hover:bg-black hover:text-white dark:text-white dark:hover:bg-white dark:hover:text-primary ${
+                                        pathname.includes('symptoms') &&
+                                        'bg-primary text-white dark:bg-primary dark:text-white'
+                                    }`}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard-plus"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="M9 14h6"/><path d="M12 17v-6"/></svg>
+                                    Symptoms
+                                </NavLink>
+                            </li>
+                            )}
+                            {/* <!-- Menu Item Medication --> */}
 
                             {/* <!-- Menu Item Appointments --> */}
                             <li>
